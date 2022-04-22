741fc292b75ad8cd2e6867f9b2653ec2a1c8a662
sarika476 <17ce133@charusat.edu.in>
2022-04-22 16:27:57 +0530

    dockerfile added

--- frontend/src/components/Admin/AdminUserinfo.js
+++ frontend/src/components/Admin/AdminUserinfo.js
@@ -1,78 +1,71 @@
 import React, { Component } from 'react' 
+import { Link } from 'react-router-dom';
 import Layout, { Content, Header } from 'antd/lib/layout/layout';
 import { Table, Tag, Space, Button } from 'antd';
+import Column from 'antd/lib/table/Column';
 import ReactDOM from 'react-dom';
 import './AdminUserinfo.css';
 
 export default class AdminUserinfo extends Component{
+
+    constructor(props) {
+        super(props)   
+        
+        this.state = {
+            api: 'http://localhost:8081/user/list',
+            records: [],
+            months : [ "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ] ,
+            targetMonth : 0,
+            id: null,
+        }
+    }
+
+    onApply = (record) => {
+        console.log(record);
+        // this.props.set_user_id(record.id);
+        sessionStorage.setItem("user_id", record.id);
+        //window.location.replace('/admin_maint_detail')
+    }
+
+    componentDidMount() {
+        fetch(this.state.api)
+        .then(response => response.json())
+        .then(records => {
+            this.setState({
+                records: records
+            })
+        })
+        .catch(error => console.log(error))
+
+    }
   render()
   {
     function edit(){
       window.location.replace('edit_userinfo');
     }
     
-    const columns = [
-      {
-        title: 'FlatNo.',
-        dataIndex: 'flatno',
-        key: 'flatno.',
-        // render: text => <a>{text}</a>,
-      },
-      {
-        title: 'Name',
-        dataIndex: 'name',
-        key: 'name',
-        // render: text => <a>{text}</a>,
-      },
-      {
-        title: 'Contact No.',
-        dataIndex: 'contact',
-        key: 'contact',
-        // render: text => <a>{text}</a>,
-      },
-      {
-        title: 'Action',
-        dataIndex: 'action',
-        key: 'action',
-        // render: (text, record) => (
-        //   <Space size="middle">
-        //     <button onClick={edit}>Edit</button>
-        //     <button>Delete</button>
-        //   </Space>
-        // ),
-      },
-    ];
     
-    const data = [
-      {
-        key: '1',
-        flatno:'A101',
-        name: 'John Brown',
-        contact:'1234567890',
-        action: <div><Button type='primary' onClick={edit}>Edit</Button><Button >Delete</Button></div>
-      },
-      {
-        key: '2',
-        flatno:'B102',
-        name: 'Jim Green',
-        contact:'1234567890',
-        action: <div><Button type='primary'>Edit</Button><Button >Delete</Button></div>
-      },
-      {
-        key: '3',
-        flatno:'A102',
-        name: 'Joe Black',
-        contact:'1234567890',
-        action: <div><Button type='primary'>Edit</Button><Button >Delete</Button></div>
-      },
-    ];
 
     return(
       
         <Layout style={{minHeight: '100vh'}}>
         <Content style={{height: '100%'}}>
           <h3 style={{padding:'20px',textAlign:'center'}}>User Information</h3>
-          <Table style={{paddingLeft:'100px',paddingRight:'100px'}} columns={columns} dataSource={data} />
+          <Table style={{paddingLeft:'100px',paddingRight:'100px'}} dataSource={this.state.records} >
+
+            <Column key="id" dataIndex={"id"} title="Flat no"/>
+            <Column key ="Name" dataIndex={"name"} title="Name"/>
+            <Column dataIndex={"contact_number"} title="Contact No."/>
+            <Column key="action" title="Action" render={(r) => {
+              return(
+                <div> 
+                  <Button type='primary' onClick={() => this.onApply(r)}>Edit</Button>
+                  <Button type='primary'>Delete</Button>
+                </div>
+              )
+            }}></Column>
+
+          </Table>
         </Content>
         </Layout>
     )   
